5e13ac7943cba1f71b670c4bfd6b079a15dcf174
Nick <nicksnow099@gmail.com>
2025-03-14 20:34:52 -0400

    change element structure of listing search

--- real-estate-app/pages/listingSearchPage.tsx
+++ real-estate-app/pages/listingSearchPage.tsx
@@ -1,3 +1,5 @@
+import styles from "@/styles/Home.module.css";
+
 import ListingsMap from "@/components/listingsMap"
 /**
  * Both map view and gallery view will be on this page. User can switch between them without changing pages.
@@ -6,12 +8,21 @@ import ListingsMap from "@/components/listingsMap"
 export default function ListingSearchPage() {
 
     return (
-        <>
+        
+            
+        <body>
             {/* header component */}
-            <body>
+
+            <main className={styles.main}>
                 {/* search, filters and button to switch between map/gallery*/}
+
                 <ListingsMap></ListingsMap>
-            </body>
-        </>
+            </main>
+
+            <footer className={styles.footer}>
+        
+            </footer>
+        </body>
+        
     )
 }
