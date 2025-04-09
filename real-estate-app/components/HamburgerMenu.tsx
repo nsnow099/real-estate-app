c570549121eb1bcbb65738798c03520bae3620db
Nick <nicksnow099@gmail.com>
2025-04-09 18:40:07 -0400

    added indicator for current page on nav menu

--- real-estate-app/components/HamburgerMenu.tsx
+++ real-estate-app/components/HamburgerMenu.tsx
@@ -1,9 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "@/styles/HamburgerMenu.module.css";
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
+  const { view } = router.query;
+  const [currPage, setPage] = useState("");
+  useEffect(() => {
+    if (view === "gallery") {
+      setPage("gallery");
+    } else if (view === "map") {
+      setPage("map");
+    } else if (view === "home") {
+      setPage("home");
+    } else if (view === "realtor") {
+      setPage("realtor");
+    }  else if (view === "news") {
+      setPage("news");
+    } else if (view === "advice") {
+      setPage("advice");
+    } else if (view === "trends") {
+      setPage("trends");
+    } else if (view === "calculator") {
+      setPage("calculator");
+    }
+  }, [view]);
 
   return (
     <div className={styles.hamburgerContainer}>
@@ -15,14 +39,30 @@ const HamburgerMenu = () => {
       {/* Dropdown Menu */}
       <div className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
         <ul>
-          <li><Link href="/">Home</Link></li>  
-          <li><Link href="/listingSearchPage?view=gallery">Gallery View</Link></li>
-          <li><Link href="/listingSearchPage?view=map">Map View</Link></li>
-          <li><Link href="/realtor?view=realtor">Realtors</Link></li>
-          <li><Link href="/analysisTools?view=news">News</Link></li>
-          <li><Link href="/analysisTools?view=advice">Advice</Link></li>
-          <li><Link href="/analysisTools?view=trends">Market Trends</Link></li>
-          <li><Link href="/analysisTools?view=calculator">Mortgage Calculator</Link></li>
+          <li className={currPage === "home" ? styles.selected : ""}>
+            <Link href="/?view=home">Home</Link>
+          </li>  
+          <li className={currPage === "gallery" ? styles.selected : ""}>
+            <Link href="/listingSearchPage?view=gallery">Gallery View</Link>
+          </li>
+          <li className={currPage === "map" ? styles.selected : ""}>
+            <Link href="/listingSearchPage?view=map">Map View</Link>
+          </li>
+          <li className={currPage === "realtor" ? styles.selected : ""}>
+            <Link href="/realtor?view=realtor">Realtors</Link>
+          </li>
+          <li className={currPage === "news" ? styles.selected : ""}>
+            <Link href="/analysisTools?view=news">News</Link>
+          </li>
+          <li className={currPage === "advice" ? styles.selected : ""}>
+            <Link href="/analysisTools?view=advice">Advice</Link>
+          </li>
+          <li className={currPage === "trends" ? styles.selected : ""}>
+            <Link href="/analysisTools?view=trends">Market Trends</Link>
+          </li>
+          <li className={currPage === "calculator" ? styles.selected : ""}>
+            <Link href="/analysisTools?view=calculator">Mortgage Calculator</Link>
+          </li>
         </ul>
       </div>
     </div>
